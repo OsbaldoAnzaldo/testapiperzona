f61b3e4e23ad9dc00cc3f3699840d31237861fd1
Osbaldo Anzaldo Segura <desarrollo1@perzona.mx>
2024-01-04 18:39:07 -0600

    swager titles

--- app/Http/Controllers/TitleController.php
+++ app/Http/Controllers/TitleController.php
@@ -37,6 +37,33 @@ class TitleController extends Controller
         return $this->respondWithSuccess($titles);
     }
 
+            /**
+     * @OA\Post(
+     * path="/api/titles",
+     * summary="Crear title",
+     * description="Se requiere name",
+     * operationId="titles store",
+     * tags={"Titles"},
+     * @OA\RequestBody(
+     *    required=true,
+     *    description="Valores",
+     *    @OA\JsonContent(
+     *       required={"name"},
+     *       @OA\Property(property="director_id", type="string", format="text", example="1"),
+     *          @OA\Property(property="name", type="string", format="text", example="Titanic"),
+     * @OA\Property(property="year", type="string", format="text", example="2023"),
+     *    ),
+     * ),
+     * @OA\Response(
+     *    response=422,
+     *    description="datos incorrectos",
+     *    @OA\JsonContent(
+     *          @OA\Property(property="validaciones", type="string", example="validaciones")
+     *        )
+     *     )
+     * )
+     */
+
     public function store(StoreTitleRequest $request)
     {
         
@@ -44,6 +71,33 @@ class TitleController extends Controller
 
         return $this->respondCreated($title);
     }
+
+    /**
+* @OA\GET(
+* path="/api/titles/id",
+* summary="Obtener title por id",
+* description="Se requiere id",
+* operationId="title get",
+* tags={"Titles"},
+* @OA\RequestBody(
+*    required=true,
+*    description="Valores",
+*    @OA\JsonContent(
+*       required={"id"},
+     *       @OA\Property(property="director_id", type="string", format="text", example="1"),
+     *          @OA\Property(property="name", type="string", format="text", example="Titanic"),
+     * @OA\Property(property="year", type="string", format="text", example="2023"),
+*    ),
+* ),
+* @OA\Response(
+*    response=404,
+*    description="datos incorrectos",
+*    @OA\JsonContent(
+*          @OA\Property(property="response", type="string", example="no encontrado")
+*        )
+*     )
+* )
+*/
 
     public function show($id)
     {
@@ -53,6 +107,31 @@ class TitleController extends Controller
         return $this->respondWithSuccess($title);
     }
 
+    /**
+ * @OA\Put(
+ * path="/api/titles/id",
+ * summary="Editar title",
+ * description="Se requiere name",
+ * operationId="titles update",
+ * tags={"Titles"},
+ * @OA\RequestBody(
+ *    required=true,
+ *    description="Valores",
+ *    @OA\JsonContent(
+ *       required={"name"},
+ *       @OA\Property(property="name", type="string", format="text", example="James Cameron"),
+ *    ),
+ * ),
+ * @OA\Response(
+ *    response=422,
+ *    description="datos incorrectos",
+ *    @OA\JsonContent(
+ *          @OA\Property(property="validaciones", type="string", example="el nombre es requerido")
+ *        )
+ *     )
+ * )
+ */
+
     public function update(UpdateTitleRequest $request, $id)
     {
         
@@ -61,6 +140,25 @@ class TitleController extends Controller
 
         return $this->respondWithSuccess($title);
     }
+
+
+     /**
+     *  @OA\Delete(
+     *      path="/titles/id",
+     *      summary="eliminar titles por id",
+     *      operationId="delete title",
+     *      tags={"Titles"},
+     *      security={{"bearer_token":{}}},
+     *      @OA\Response(
+     *          response=200,
+     *          description=" registro eliminado."
+     *      ),
+     *      @OA\Response(
+     *          response="404",
+     *          description="no encontrrado."
+     *      )
+     *  )
+     */
 
     public function destroy($id)
     {
